bbf15af0497d8f9796330bdfd34cd3c59b0488b4
Ryan King <ryan@helloworldeng.com>
2019-10-10 08:17:42 -0700

    NPC states have a more flexible sound effect system

--- Assets/Scripts/NPCs/RobotController.cs
+++ Assets/Scripts/NPCs/RobotController.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class RobotController : MonoBehaviour
 {
-   //Stuff Nick added for audio
-    public FMODUnity.StudioEventEmitter roboMove;
-
+    /** Type Definitions */ 
     // TODO: If this stuff gets complicated, we can do an actual state machine
     // Enum is fine for now
+    [System.Serializable]
     enum State
     {
         Default,
         LookAtPlayer
     }
+
+    // Holds information about how each state behaves
+    // This could just be an actual state machine but w/e
+    [System.Serializable]
+    struct StateInfo
+    {
+        // Because unity can't serialize a dictionary, we are putting the relevant state in the StateInfo itself
+        public State state;
+        public FMODUnity.StudioEventEmitter enterSoundEmitter;
+        public FMODUnity.StudioEventEmitter exitSoundEmitter;
+        public FMODUnity.StudioEventEmitter loopingSoundEmitter;
+    }
+
+    /** Public Vars */
+
+    /** Private Vars */
+
+    [SerializeField]
+    private List<StateInfo> stateInfos = new List<StateInfo>();
 
     [SerializeField]
     private float lookAtPlayerLerpSpeed = 1;
@@ -23,10 +41,7 @@ public class RobotController : MonoBehaviour
     // Start is called before the first frame update
     public void LookAtPlayer()
     {
-        state = State.LookAtPlayer;
-        //play audio when looking at player
-        roboMove.Play();
-        roboMove.SetParameter("TurnOff", 1);
+        SetState(State.LookAtPlayer);
     }
 
     void Update()
@@ -47,6 +62,41 @@ public class RobotController : MonoBehaviour
         }
     }
 
+    void SetState(State state)
+    {
+        DidExitState(state);
+        this.state = state;
+        DidEnterState(state);
+    }
+
+    void DidEnterState(State state)
+    {
+        var info = GetStateInfoForState(state);
+        if (info.enterSoundEmitter != null)
+        {
+            info.enterSoundEmitter.Play();
+            info.enterSoundEmitter.SetParameter("TurnOff", 1);
+        }
+        if (info.loopingSoundEmitter != null)
+        {
+            // TODO: Play the looping sound emitter
+        }
+    }
+
+    void DidExitState(State state)
+    {
+        var info = GetStateInfoForState(state);
+        if (info.exitSoundEmitter != null)
+        {
+            info.exitSoundEmitter.Play();
+            info.exitSoundEmitter.SetParameter("TurnOff", 1);
+        }
+        if (info.loopingSoundEmitter != null)
+        {
+            // TODO: Stop the looping sound emitter
+        }
+    }
+
     Vector3 GetPlayerPosition()
     {
         var player = Object.FindObjectOfType<PlayerController>();
@@ -57,5 +107,20 @@ public class RobotController : MonoBehaviour
         }
 
         return player.transform.position;
+    }
+
+    StateInfo GetStateInfoForState(State state)
+    {
+        foreach(var stateInfo in stateInfos)
+        {
+            if(stateInfo.state == state)
+            {
+                return stateInfo;
+            }
+        }
+
+        // Return empty info if there is none
+        Debug.LogWarning("No state info specified for state " + state + " on object " + this);
+        return new StateInfo();
     }
 }
